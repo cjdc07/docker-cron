c5cf51eb1c0fb260505adca6b55a70b83fc45fa1
CJ Catalan <cj.catalan07@gmail.com>
2024-02-12 13:25:17 -0500

    add script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,17 @@
 
 env >> /etc/environment
 
-printenv
+auth_response=$(curl -X POST -H "Content-Type: application/json" -d '{
+  "query": "mutation AuthenticateUser($email: String!, $password: String!) { authenticateUserWithPassword(email: $email, password: $password) { ... on UserAuthenticationWithPasswordSuccess { sessionToken } } }",
+  "variables": {
+    "email": "$BOT_EMAIL",
+    "password": "$BOT_PASSWORD"
+  }
+}' $CMS_URL/api/graphql)
+
+session_token=$(echo "$auth_response" | grep -o '"sessionToken":"[^"]*' | awk -F ':"' '{print $2}')
+
+curl -X POST -H "Content-Type: application/json" --cookie "keystonejs-session=$session_token" $CMS_URL/rest/daily-product-count
 
 # execute CMD
 echo "$@"
